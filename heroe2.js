036023e4a605685695b608ddf027c79756065ad9
carlos <carlos.campusano15042007@gmail.com>
2026-02-28 10:17:31 -0500

    creando heroe2.js

---
+++ heroe2.js
@@ -0,0 +1,12 @@
+let heroe2 = "spiderman";
+console.log(heroe2);
+let poder = "fuerza sobnehumana";
+console.log(poder);
+let villano = "venom";
+console.log(villano);
+
+
+
+let cuento = "mi nombre es spiderman y mi poder es la fuerza sobnehumana y mi villano es venom";
+console.log(cuento);
+
